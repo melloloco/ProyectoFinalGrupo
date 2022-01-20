f4879afc1a58e7213ea837fa10064e0f3b3e85f1
Alejandro Sánchez-Arjona Balado <alejandro98sab@gmail.com>
2022-01-20 12:31:56 +0100

    Created the register link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogoComponent } from './catalogo/catalogo.component';
 import { HomeComponent, PageNotFoundComponent } from './main';
+import { RegisterUserComponent } from './security/register-user/register-user.component';
 import { AuthGuard } from './security';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full'},
   { path: 'inicio', component: HomeComponent},
   { path: 'catalogo', component: CatalogoComponent, data: {pageTitle:'catalogo'}},
+  { path: 'registro', component: RegisterUserComponent, data: {pageTitle:'registro'}},
   { path: 'config', loadChildren: () => import('./config/config.module').then(mod => mod.ConfigModule)},
   { path: '404.html', component: PageNotFoundComponent},
   { path: '**', component: PageNotFoundComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { CommonServicesModule } from './common-services';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CatalogoComponent } from './catalogo/catalogo.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RegisterUserComponent } from './security/register-user/register-user.component';
 
 @NgModule({
   declarations: [
