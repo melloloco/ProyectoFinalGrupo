55929ece5f5bbab244e7b3f10214d5a2c4558869
Fernandopy <Fernandoabaran@gmail.com>
2022-01-20 12:11:51 +0100

    inicio catalogo2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { AuthGuard } from './security';
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full'},
   { path: 'inicio', component: HomeComponent},
-  { path: 'catalogo', component: CatalogoComponent, data: {pageTitle:'catalogo'}},
+  { path: 'catalogo', component: CatalogoComponent, data: {pageTitle:'Catalogo'}},
   { path: 'config', loadChildren: () => import('./config/config.module').then(mod => mod.ConfigModule)},
   { path: '404.html', component: PageNotFoundComponent},
   { path: '**', component: PageNotFoundComponent},
