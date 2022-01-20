8a472d408751f83bb0a26e6e0a03b926bc4f095c
Alejandro Sánchez-Arjona Balado <alejandro98sab@gmail.com>
2022-01-20 13:02:18 +0100

    Added the login  - Changed the routes to match both, register and login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CatalogoComponent } from './catalogo/catalogo.component';
 import { HomeComponent, PageNotFoundComponent } from './main';
 import { RegisterUserComponent } from './security/register-user/register-user.component';
+import { LoginComponent } from './security/login/login.component';
 import { AuthGuard } from './security';
 
 const routes: Routes = [
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'inicio', component: HomeComponent},
   { path: 'catalogo', component: CatalogoComponent, data: {pageTitle:'catalogo'}},
   { path: 'registro', component: RegisterUserComponent, data: {pageTitle:'registro'}},
+  { path: 'login', component: LoginComponent, data: {pageTitle:'login'}},
   { path: 'config', loadChildren: () => import('./config/config.module').then(mod => mod.ConfigModule)},
   { path: '404.html', component: PageNotFoundComponent},
   { path: '**', component: PageNotFoundComponent},
