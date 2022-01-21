d68a2f55cf763aa130621241e14b6aa5408cde21
Javier <javicue200@hotmail.com>
2022-01-21 11:57:49 +0100

    modulo ingredientes

---
+++ src/app/Ingredientes/Ingredientes.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IngredientesComponent } from './ingredientes/ingredientes.component';
+
+
+
+@NgModule({
+  declarations: [
+    IngredientesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class IngredientesModule { }
